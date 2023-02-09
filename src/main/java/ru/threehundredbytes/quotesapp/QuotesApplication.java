07cefd23cf6da72c22ad735548fdba7826fe9ee4
Anton Kosarev <antonkosarev.dev@gmail.com>
2023-02-09 10:49:42 +0200

    Initial commit

---
+++ src/main/java/ru/threehundredbytes/quotesapp/QuotesApplication.java
@@ -0,0 +1,13 @@
+package ru.threehundredbytes.quotesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesApplication.class, args);
+	}
+
+}
